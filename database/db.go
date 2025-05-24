58a6cec9ee585e2b97bf1c7a7c0c11b1ebbcd7a7
zavieruka <zvkmateus@gmail.com>
2025-05-24 10:18:49 -0300

    Add database connection and Docker configuration for PostgreSQL

---
+++ database/db.go
@@ -0,0 +1,23 @@
+package database
+
+import (
+	"go-gin-api/models"
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB  *gorm.DB
+	err error
+)
+
+func ConnectWithDB() {
+	stringDeConexao := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	DB, err = gorm.Open(postgres.Open(stringDeConexao))
+	if err != nil {
+		log.Panic("Could not connect to the database")
+	}
+	DB.AutoMigrate(&models.Student{})
+}
